f6557ba3eb2bf8603aa0c3c1db15092b34330775
jhon mendez <fullstackuc@gmail.com>
2026-07-21 21:38:41 -0500

    feat:add new example html,css and js v3

--- 1.web/script.js
+++ 1.web/script.js
@@ -6,7 +6,11 @@ let agregarTareaBtn = document.getElementById("add-btn");
 let tareaTxt = document.getElementById("todo-input");
 let eliminarTareaBtn = document.getElementById("eliminarTarea");
 const inputTxt = document.getElementById("todo-input");
-
+let filtroActual = "all";
+let filtroAll = document.getElementById("filter-all");
+let filtroActive = document.getElementById("filter-active");
+let filtroCompleted = document.getElementById("filter-completed");
+let filtroClear = document.getElementById("clear-completed");
 /*
 let titulo = document.getElementsByTagName("h1")[0];
 
@@ -43,6 +47,38 @@ inputTxt.addEventListener("keydown", (event) => {
   }
 });
 
+todolist.addEventListener("click", (e) => {
+  if (e.target.tagName == "INPUT" || e.target.tagName == "SPAN") {
+    const index = e.target.dataset.index;
+    tareas[index].estado = !tareas[index].estado;
+  }
+});
+
+filtroAll.addEventListener("click", () => {
+  filtroActual = "all";
+  renderizarTareas();
+});
+
+filtroActive.addEventListener("click", () => {
+  filtroActual = "active";
+  renderizarTareas();
+});
+
+filtroCompleted.addEventListener("click", () => {
+  filtroActual = "completed";
+  renderizarTareas();
+});
+
+filtroCompleted.addEventListener("click", () => {
+  filtroActual = "completed";
+  renderizarTareas();
+});
+
+filtroClear.addEventListener("click", () => {
+  tareas = tareas.filter((t) => !t.estado);
+  renderizarTareas();
+});
+
 function agregarTarea(tarea) {
   tareas.push(tarea);
 }
@@ -54,11 +90,20 @@ function eliminarTarea() {
 function renderizarTareas() {
   //qué quiero renderizar?
   todolist.innerHTML = "";
-  tareas.forEach((tarea) => {
+  let tareasFiltro = tareas.filter((t) => {
+    if (filtroActual === "active") return !t.estado;
+    if (filtroActual === "completed") return t.estado;
+    return true;
+  });
+
+  tareasFiltro.forEach((tarea, index) => {
+    console.log(tarea, index);
     todolist.innerHTML += `
     <li>
        <div class="todo-item">
-         <input type="checkbox" ${tarea.estado ? "checked" : ""} />
+         <input type="checkbox" ${tarea.estado ? "checked" : ""} 
+         data-index="${index}"
+         />
          <span>${tarea.nombre}</span>
        </div>
       <button id="eliminarTarea">✖</button>
